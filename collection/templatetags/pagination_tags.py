57e8762fd1afc586976f6f3d90e202eda0c36bc2
hieu356xz <kotarou2k4@gmail.com>
2025-03-18 04:43:59 +0700

    Add collection page and pagination

---
+++ collection/templatetags/pagination_tags.py
@@ -0,0 +1,20 @@
+# yourapp/templatetags/pagination_tags.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def page_range(current_page, total_pages):
+    current_page = int(current_page)
+    total_pages = int(total_pages)
+    print(current_page, total_pages)
+    
+    start = max(1, current_page - 2)
+    end = min(total_pages, current_page + 2)
+    print(start, end)
+    
+    return range(start, end + 1)
+
+@register.filter
+def to_int(value):
+    return int(value)
