1a0133ebcfc527fc758db5a581a46ed9072956bd
hieu356xz <kotarou2k4@gmail.com>
2025-03-17 16:04:34 +0700

    Refactor Book model to support multiple authors through BookAuthor model and update admin interface

--- product/models.py
+++ product/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=511)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=15, decimal_places=2)
     publish_year = models.PositiveSmallIntegerField(blank=True, null=True)
     cover_img = models.ImageField(upload_to='covers/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=255, unique=True)
+    authors = models.ManyToManyField('Author', through='BookAuthor')
     attributes = models.ManyToManyField('Attribute', through='BookAttributeValue')
     genres = models.ManyToManyField('Genre', through='BookGenre')
     collections = models.ManyToManyField('collection.Collection', through='collection.BookCollection')
@@ -24,6 +24,17 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
+class BookAuthor(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['book', 'author'], name='unique_book_author_migration'),
+        ]
+
+    def __str__(self):
+        return self.book.title + ' - ' + self.author.name
 class Publisher(models.Model):
     name = models.CharField(max_length=511)
     address = models.TextField(blank=True, null=True)
