468545d14999c1038ed20e008dac6d3e8b4af52a
hieu356xz <kotarou2k4@gmail.com>
2025-03-17 22:58:06 +0700

    Add product detail page

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("<str:slug>/", views.product_detail, name="product_detail"),
-    path("", views.index)
+    path("<str:slug>/", views.product_detail, name="product_detail"),
 ]
