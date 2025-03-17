61d5c55af626e6fddfd099d438995f11fb1e7094
hieu356xz <kotarou2k4@gmail.com>
2025-03-18 01:53:15 +0700

    Add home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+from product.models import Book
+
 def index(request):
-    return render(request, 'home.html')
+    new_products = Book.objects.all().order_by("-pk")[:10]
+    best_selling_products = Book.objects.all()[:10]
+    vietnamese_literatures = Book.objects.filter(collection__name="Văn học Việt Nam")[:10]
+    manga_comics = Book.objects.filter(collection__name="Manga - Comic")[:10]
+    light_novels = Book.objects.filter(collection__name="Light Novel")[:10]
+
+    print(new_products)
+    context = {
+        "new_products": new_products,
+        "best_selling_products": best_selling_products,
+        "vietnamese_literatures": vietnamese_literatures,
+        "manga_comics": manga_comics,
+        "light_novels": light_novels
+    }
+    return render(request, 'home/home.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'home/about.html')
