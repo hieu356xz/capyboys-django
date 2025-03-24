a992ed25add2e829b3765c497aacd7e5ff7480f2
hieu356xz <kotarou2k4@gmail.com>
2025-03-24 16:08:57 +0700

    Add admin site for Blog and BlogType

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blog, BlogType
 
-# Register your models here.
+class BlogTypeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content', 'description']
+    list_filter = ['blog_type']
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(BlogType, BlogTypeAdmin)
