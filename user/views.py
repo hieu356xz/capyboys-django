3a8fe760c7aa79c6f9afe3f56069a47040579436
hieu356xz <kotarou2k4@gmail.com>
2025-03-28 22:25:39 +0700

    Add login view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    email = ""
+
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+        else:
+            email = form.data["username"]
+            messages.error(request, "Email hoặc mật khẩu không đúng")
+    
+    context = {
+        "email": email,
+    }
+    return render(request, "user/login.html", context)
