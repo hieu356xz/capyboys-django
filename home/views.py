16292907ef6732cc72bfc1ba06c3e76947da189c
hieu356xz <kotarou2k4@gmail.com>
2025-03-30 14:43:02 +0700

    Add new blogs section in home view, refactor blog item and blog list

--- home/views.py
+++ home/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.db.models import Q
 from product.models import Book
+from blog.models import Blog
 
 def index(request):
     new_products = Book.objects.all().order_by("-pk")[:10]
-    best_selling_products = Book.objects.all()[:10]
-    vietnamese_literatures = Book.objects.filter(collection__name="Văn học Việt Nam")[:10]
-    manga_comics = Book.objects.filter(collection__name="Manga - Comic")[:10]
-    light_novels = Book.objects.filter(collection__name="Light Novel")[:10]
+    best_selling_products = Book.objects.all()[:10] # Placeholder
+    vietnamese_literatures = Book.objects.filter(collection__slug="van-hoc-viet-nam").order_by("-pk")[:10]
+    manga_comics = Book.objects.filter(collection__slug="manga-comic").order_by("-pk")[:10]
+    light_novels = Book.objects.filter(collection__slug="light-novel").order_by("-pk")[:10]
+    new_blogs = Blog.objects.all().order_by("-publish_date")[:5]
 
     context = {
         "new_products": new_products,
         "best_selling_products": best_selling_products,
         "vietnamese_literatures": vietnamese_literatures,
         "manga_comics": manga_comics,
-        "light_novels": light_novels
+        "light_novels": light_novels,
+        "new_blogs": new_blogs,
     }
     return render(request, 'home/home.html', context)
 
@@ -25,7 +28,6 @@ def search(request):
     PAGE_SIZE = 24
     query = request.GET.get("q")
     page = int(request.GET.get('page', default=1))
-    print(page)
 
     start = max((page - 1) * PAGE_SIZE, 0)
     end = start + PAGE_SIZE
