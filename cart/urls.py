0fb5d91a3992ea0ceb5a710e2a9008ef2fe99023
hieu356xz <kotarou2k4@gmail.com>
2025-03-31 04:14:08 +0700

    Implement cart item removal and quantity update functionality

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("add/", views.cart_add, name="add-to-cart"),
-    path("view/", views.cart_modal_view, name="cart-modal")
+    path("view/", views.cart_modal_view, name="cart-modal"),
+    path("remove/", views.cart_remove_item, name="remove-from-cart"),
+    path("update/", views.cart_update_quantity, name="update-cart-item-qty"),
 ]
