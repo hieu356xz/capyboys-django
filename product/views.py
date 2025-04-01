818fffb28c8c413c5b895ed28e5118d3224a11f7
hieu356xz <kotarou2k4@gmail.com>
2025-04-01 22:30:07 +0700

    Refactor product view and add related products section in product detail

--- product/views.py
+++ product/views.py
@@ -3,28 +3,55 @@ from django.http import Http404
 from cart.cart import CartSession
 from product.models import Book, BookAttributeValue
 
-# Create your views here.
+# exclude general collections
+exclude_collections = (
+    "lich-su-truyen-thong",
+    "van-hoc-viet-nam",
+    "truyen-tranh",
+    "manga-comic",
+    "light-novel",
+    "kien-thuc-khoa-hoc",
+)
+
 def product_detail(request, slug):
     try:
         cart = CartSession(request)
-        product = Book.objects.get(slug=slug)
-        product_attributes = BookAttributeValue.objects.filter(book=product).order_by('attribute__pk')
-        collection = product.collections.first()
-        formatted_price = f"{product.price:,.0f} ₫"
+        product = Book.objects.select_related('publisher').prefetch_related(
+            'collections',
+            'authors',
+            'genres',
+            'bookattributevalue_set'
+        ).get(slug=slug)
+        
+        product_attributes = product.bookattributevalue_set.all()
+        current_collection = product.collections.filter(slug=slug).first()
+        collections = product.collections.exclude(slug__in=exclude_collections)
         authors = product.authors.all()
+        genres = product.genres.all()
+
+        related_products = Book.objects.filter(authors__in=authors).exclude(pk=product.id).distinct()
+        related_genre_products = Book.objects.filter(genres__in=genres).exclude(pk=product.id).distinct()
+
+        formatted_price = f"{product.price:,.0f} ₫"
         max_quantity = max(0, min(product.stock - cart.get_item_quantity(product.pk), 99))
         min_quantity = 1 if max_quantity else 0
+
     except Book.DoesNotExist:
         raise Http404("Product does not exist")
 
     texmplate_name = 'product/product_detail.html'
     context = {
         "product": product,
-        "collection": collection, 
+        "current_collection": current_collection,
+        "collections": collections,
         "product_price": formatted_price,
         "product_attributes": product_attributes,
         "authors": authors,
+        'genres': genres,
         "max_quantity": max_quantity,
         "min_quantity": min_quantity,
+        "related_products": related_products,
+        "related_genre_products": related_genre_products,
     }
+
     return render(request, texmplate_name, context)
