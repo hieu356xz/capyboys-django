07d91f04c7a98f58766bac9bc74d5b649ec627d6
hieu356xz <kotarou2k4@gmail.com>
2025-04-02 22:17:38 +0700

    Add remove_query_param filter, refactor add_query_param to set_query_param

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -2,6 +2,7 @@
 from urllib.parse import urlparse, parse_qs
 from django import template
 from home.utils import QueryParams
+import html
 
 register = template.Library()
 
@@ -16,7 +17,8 @@ def get_page_range(current_page, total_pages):
     return range(start, end + 1)
 
 @register.filter
-def add_query_param(url: str, param: str):
+def set_query_param(url: str, param: str):
+    url = html.unescape(url)
     parsed_url = urlparse(url)
     query_dict = { key: value[-1] for key, value in parse_qs(parsed_url.query).items()}
     query_params = QueryParams(query_dict)
@@ -24,6 +26,15 @@ def add_query_param(url: str, param: str):
     param_name, param_value = param.split('=', 1)
     query_params.set(param_name, param_value)
     return query_params.build_url(parsed_url.path)
+
+@register.filter
+def remove_query_param(url: str, param: str):
+    url = html.unescape(url)
+    parsed_url = urlparse(url)
+    query_dict = { key: value[-1] for key, value in parse_qs(parsed_url.query).items()}
+    query_params = QueryParams(query_dict)
+
+    return query_params.without(param).build_url(parsed_url.path)
 
 @register.filter
 def calc_subtotal(price, count):
