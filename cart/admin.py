1e6e94e9bc370ab7e5824c86186a26a429134935
hieu356xz <kotarou2k4@gmail.com>
2025-04-03 19:00:11 +0700

    Fix Cart and CartItem save method, add admin page for Cart and Order

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Order, OrderItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+    autocomplete_fields = ('book', 'cart')
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name')
+
+    readonly_fields = ('created_at', 'updated_at')
+
+    inlines = [CartItemInline]
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+    readonly_fields = ('price', 'discount', 'final_price')
+    autocomplete_fields = ('book', 'order')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'address', 'status', 'payment_method', 'payment_status', 'created_at')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name')
+
+    readonly_fields = ('created_at', 'updated_at', 'total_price')
+
+    inlines = [OrderItemInline]
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
