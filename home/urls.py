ab3bb38e7bbe9cee38bf3a8b5f096ffd58e72dbc
hieu356xz <kotarou2k4@gmail.com>
2025-04-06 22:38:44 +0700

    Add checkout success view

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("search/", views.search, name="search"),
     path("checkout/", views.checkout, name="checkout"),
+    path("checkout/success/", views.checkout_success, name="checkout_success"),
 ]
