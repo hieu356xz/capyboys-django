56c46cce2cf57be74c9b5b17b97ae8c3ec3f595e
hieu356xz <kotarou2k4@gmail.com>
2025-04-09 01:05:15 +0700

    Add email configuration settings

--- capyboys_django/settings.py
+++ capyboys_django/settings.py
@@ -194,3 +194,11 @@ else:
             'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
         },
     }
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = env('EMAIL_PORT', default=587)
+EMAIL_USE_TLS = env('EMAIL_USE_TLS', default=True)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
