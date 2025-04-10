0c4eca5d510002cc7714f6330003481c05adf5c0
hieu356xz <kotarou2k4@gmail.com>
2025-04-10 18:05:50 +0700

    Add password change done template

--- user/urls.py
+++ user/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name="password_reset_complete"),
     path("password_change/", auth_views.PasswordChangeView.as_view(template_name='user/password_change.html'), name="password_change"),
-    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(template_name='user/password_change_done.html'), name="password_change_done"),
     path("orders/<int:order_id>/", views.order_detail, name="order_detail"),
 ]
