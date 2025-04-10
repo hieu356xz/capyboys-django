ec1f3ec04bed9949da8c52ac11cb7df1d897c371
hieu356xz <kotarou2k4@gmail.com>
2025-04-10 17:33:41 +0700

    Add order detail view

--- user/urls.py
+++ user/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name="password_reset_complete"),
+    path("orders/<int:order_id>/", views.order_detail, name="order_detail"),
 ]
